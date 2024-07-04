05e9da54b6c7d7af877be52d41292192ea6e19b1
Izairton Oliveira de Vasconcelos <iovascon@gmail.com>
2024-07-04 16:50:47 -0300

    Subindo o projeto completo para o GitHub

---
+++ ExemploTDD/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ExemploTDD
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var calculadora = new Calculadora();
+            System.Console.WriteLine($"2 + 3 = {calculadora.Adicionar(2, 3)}");
+            System.Console.WriteLine($"5 - 3 = {calculadora.Subtrair(5, 3)}");
+        }
+    }
+}
